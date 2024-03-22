9cb1cfbec9abb7ce6ebd003c8a002db9f18d9ee0
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-03-23 02:59:34 +0300

    .

--- lua/safi/plugins/lsp-config.lua
+++ lua/safi/plugins/lsp-config.lua
@@ -6,17 +6,6 @@ end
 
 local protocol = require('vim.lsp.protocol')
 
-local on_attach = function(client, bufnr)
-  -- format on save
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("Format", { clear = true }),
-      buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
-    })
-  end
-end
-
 lspconfig["dartls"].setup({
   on_attach = on_attach,
 	settings = {
